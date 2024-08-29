a540481108ed51f761223dbc918b920013d8d693
Kyle Hubbs <khubb008@odu.edu>
2024-08-29 19:35:41 -0400

    Adjusted navbar js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const header = document.querySelector('header');
 
-window.addEventListener('scroll', function() {
-    header.classList.toggle('sticky', window.scrollY > 120);
-});
+// window.addEventListener('scroll', function() {
+//     header.classList.toggle('sticky', window.scrollY > 120);
+// });
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
